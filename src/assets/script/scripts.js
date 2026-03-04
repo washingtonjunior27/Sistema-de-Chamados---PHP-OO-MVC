8430084be941efd449ad2a2f8af45c9958b5e8b4
Washington Junior <washjunior4444@gmail.com>
2026-03-03 22:03:31 -0400

    update home admin page

---
+++ src/assets/script/scripts.js
@@ -0,0 +1,21 @@
+var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 1,
+      grabCursor: true,
+      spaceBetween: 20,
+      breakpoints: {
+        
+        // when window width is >= 640px
+        485: {
+            slidesPerView: 2
+        },
+        987: {
+            slidesPerView: 3
+        },
+        1200: {
+            slidesPerView: 4
+        },
+        1400: {
+            slidesPerView: 5
+        }
+    }
+});
